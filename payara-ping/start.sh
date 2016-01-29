c29a391179bf45190fd9d99588462f6ea81028ff
Adam Bien <abien@adam-bien.com>
2016-01-29 05:31:00 +0100

    start script removed

--- payara-ping/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo '------- Starting ${PAYARA_ARCHIVE}  -------'
-/opt/payara41/bin/asadmin start-domain ${DOMAIN_NAME}
-/opt/payara41/bin/asadmin deploy --force=true ping.war
-tail -f /opt/payara41/glassfish/domains/${DOMAIN_NAME}/logs/server.log
