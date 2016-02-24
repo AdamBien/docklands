02006d9294788ed5312c4a470581c2369955f8e1
Adam Bien <abien@adam-bien.com>
2016-02-24 15:47:09 +0100

    PWD removed

--- nano/build.js
+++ nano/build.js
@@ -1,5 +1,4 @@
 #!/usr/bin/jjs -fv
-$ENV.PWD = "nano";
 var cmd = "docker build --no-cache -t airhacks/nano ."
 var System = Java.type("java.lang.System");
 $EXEC(cmd, System.in, System.out, System.err);
