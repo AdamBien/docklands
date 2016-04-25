865b56f714d5908df6fc0ddcdbcc06c835af7771
Adam Bien <abien@adam-bien.com>
2016-04-25 09:21:32 +0200

    Dropin images renamed

--- tomee-plus-dropin/build.js
+++ tomee-plus-dropin/build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/jjs -fv
-var cmd = "docker build -t airhacks/dropin-tomee-plus ."
+var cmd = "docker build -t airhacks/tomee-plus-dropin ."
 var System = Java.type("java.lang.System");
 $EXEC(cmd, System.in, System.out, System.err);
