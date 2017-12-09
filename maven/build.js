615e5e2262610a86981ff4bcce19400047da0793
Adam Bien <abien@adam-bien.com>
2017-12-09 15:00:27 +0100

    build.js fixed

--- maven/build.js
+++ maven/build.js
@@ -1,4 +1,4 @@
 #!/usr/bin/jjs -fv
-var cmd = "docker build -t airhacks/archiva ."
+var cmd = "docker build -t airhacks/maven ."
 var System = Java.type("java.lang.System");
 $EXEC(cmd, System.in, System.out, System.err);
