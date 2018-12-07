a1f10a496730b76381299807a0199c7c688a65dd
Adam Bien <abien@adam-bien.com>
2018-12-07 15:07:01 +0100

    swarm sample removed

--- wildfly-hollow-swarm/build.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/jjs -fv --language=es6
-const cmd = "docker build -t airhacks/wildfly-hollow-swarm ."
-const System = Java.type("java.lang.System");
-$EXEC(cmd, System.in, System.out, System.err);
