c82d8ae9c28f3b86a4d9d14f4f07789a8b7c9017
Adam Bien <abien@adam-bien.com>
2020-04-27 13:11:07 +0200

    Updated to Payara 5.201 on Java 11

--- payara/start.sh
+++ payara/start.sh
@@ -2,4 +2,4 @@
 echo 'Starting Payara'
 echo deploy ${DEPLOYMENT_DIR}/`cd ${DEPLOYMENT_DIR} && ls -1 | head -1` > ${INSTALL_DIR}/deploy-command
 cat ${INSTALL_DIR}/deploy-command
-asadmin start-domain --verbose --postbootcommandfile ${INSTALL_DIR}/deploy-command ${DOMAIN_NAME}
+./asadmin start-domain --verbose --postbootcommandfile ${INSTALL_DIR}/deploy-command ${DOMAIN_NAME}
